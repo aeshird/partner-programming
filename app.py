a3a9010a57aedd9b7d9b4a98224243ca13bee10b
aeshird <ainsley-shird@uiowa.edu>
2024-04-05 14:27:54 -0500

    survey work

--- app.py
+++ app.py
@@ -5,14 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html',
-                           age_mapping=age_mapping, 
-                           gender_mapping=gender_mapping,
-                           country_mapping=country_mapping,
-                           highest_deg_mapping=highest_deg_mapping,
-                           code_experience_mapping=code_experience_mapping,
-                           current_title_mapping=current_title_mapping,
-                           company_size_mapping=company_size_mapping)
+    return render_template('index.html')
 
 @app.route('/aboutme')
 def aboutme():
